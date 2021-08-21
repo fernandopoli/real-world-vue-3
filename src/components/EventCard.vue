19b35b4421d57d731b46cff972b9d879dd434066
Fernando Poli <fernandoandrespoli@gmail.com>
2021-08-20 22:46:41 -0300

    main: First Commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.event {
+  &-card {
+    margin-bottom: 30px;
+    width: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid;
+    padding: 30px;
+    h4 {
+      margin-bottom: 0px;
+    }
+  }
+}
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="events">
+    <h2>Events for Good</h2>
+    <EventCard v-for="(event, index) in events" :key="index" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          date: '13/08/1981',
+          time: '18:00',
+          title: 'Prueba 01',
+        },
+        {
+          date: '13/08/1982',
+          time: '18:00',
+          title: 'Prueba 02',
+        },
+        {
+          date: '13/08/1983',
+          time: '18:00',
+          title: 'Prueba 03',
+        },
+        {
+          date: '13/08/1984',
+          time: '18:00',
+          title: 'Prueba 04',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style scoped lang="scss">
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
